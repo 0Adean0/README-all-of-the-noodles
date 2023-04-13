5c9d9657ddb1f40382102cadf7dd908ec5eb772f
MuheCa <119535228+0Adean0@users.noreply.github.com>
2023-04-12 21:06:45 -0400

    made final adjustments without badges

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-# README-all-of-the-noodles
+# s undefined
+# README.md-all-of-the-noodles
+
+## Description
+
+s
+
+## Table of Contents
+
+-[Installation](#installation)
+-[Usage](#usage)
+-[Credits](#credits)
+-[License](#license)
+-[Contributions](#contributions)
+-[Tests](#tests)
+-[Questions](#questions)
+
+## Installation
+s
+## Usage
+s
+## Credits
+
+## License
+The undefined serves as the open use basis for this project.
+## Contributions
+s
+## Tests
+s
+## Questions
+[Attatched is the embedded link to my GitHub](https://github.com/undefined)
+
+If you have further questions, email me freely at s 
--- index.js
+++ index.js
@@ -1,65 +1,66 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer')
 const fs = require('fs')
-const generateMarkdown = require('./utils/generateMarkdown.js');
+const generateMarkdown = require('./utils/generateMarkdown')
 
 // TODO: Create an array of questions for user input
-const questions = [
+const questions = inquirer.prompt([
 {
     type:'input',
     message:'What is the name of this project ?',
-    name:'Title',
+    name:'title',
 },
 {
     type:'input',
     message:'Enter the project description.',
-    name:'Description',
+    name:'description',
 },
 {
     type:'input',
     message:'What are the installation instructions for the project, if any ?',
-    name:'Installation',
+    name:'installation',
 },
 {
     type:'input',
     message:'What are the usage instructions for the project ?',
-    name:'Usage',
+    name:'usage',
 },
 {
     type:'input',
     message:'Was there any collaboration don or materials sourced to complete this project ?',
-    name:'Credits',
+    name:'credits',
 },
 {
     type:'input',
     message:'What are the contribution guidelines for this project, if any ?',
-    name:'Contribution',
+    name:'contributions',
 },
 {
     type:'input',
     message:'What are the test guidelines for this project, if any ?',
-    name:'Test',
+    name:'tests',
 },
 {
     type:'list',
     message:'What is the license that will be attatched to this project ?',
-    options: ['MIT License','Mozilla Public License 2.0','Apache License 2.0','Boost Software License 1.0'],
-    name:'License',
+    choices: ['MIT License','Mozilla Public License 2.0','Apache License 2.0','Boost Software License 1.0'],
+    name:'license',
 },
 {
     type:'input',
     message:'What is your GitHub username attatched to this project ?',
-    name:'Github',
+    name:'github',
 },
 {
     type:'input',
     message:'What is your email address associated with this project ?',
-    name:'Email',
+    name:'email',
 },
 
-];
+]).then((response) => createFile(response))
+
 // TODO: Create a function to write README file
-function createFile (data) {
+ function createFile (data) {
     const readMeInput = generateMarkdown(data)
     fs.writeFile('README.md',readMeInput, (err) =>
     err ? console.error(err) : console.log("README.md is complete, check the .md file for finished product")
@@ -70,5 +71,3 @@ const init = function () {
     inquirer.createPromptModule(questions)
     .then((data) => createFile(data))
 }
-
-init()
